11feb25789a4d7a68e6bd6954266827b9910a5d9
nlhan <nianlonghan@gmail.com>
2018-08-08 19:41:26 +0800

    move main function into cpp

--- ObserverPatternDemo/ObserverPatternDemo.cpp
+++ ObserverPatternDemo/ObserverPatternDemo.cpp
@@ -1,2 +1,21 @@
 #include "ObserverPatternDemo.h"
 
+int main()
+{
+    InstanceSubject *insSubject = new InstanceSubject;
+    Observer *ob1 = new Observer1;
+    Observer *ob2 = new Observer2;
+
+    insSubject->RegisterObservers(ob1);
+    insSubject->RegisterObservers(ob2);
+
+    insSubject->NotifyObservers();
+
+    delete insSubject;
+    delete ob1;
+    delete ob2;
+
+    system("PAUSE");
+
+    return 0;
+}
--- ObserverPatternDemo/ObserverPatternDemo.h
+++ ObserverPatternDemo/ObserverPatternDemo.h
@@ -76,23 +76,3 @@ public:
     ~Observer2() {}
     virtual void Update() { cout << "Current Observer is 2" << endl; }
 };
-
-int main()
-{
-    InstanceSubject *insSubject = new InstanceSubject;
-    Observer *ob1 = new Observer1;
-    Observer *ob2 = new Observer2;
-
-    insSubject->RegisterObservers(ob1);
-    insSubject->RegisterObservers(ob2);
-
-    insSubject->NotifyObservers();
-
-    delete insSubject;
-    delete ob1;
-    delete ob2;
-
-    system("PAUSE");
-
-    return 0;
-}
